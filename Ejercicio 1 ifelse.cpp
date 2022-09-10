425a0b7268958656794b9c827433e0b0368aa955
CGX <92705245+Cyberghxst@users.noreply.github.com>
2022-09-09 19:46:11 -0500

    Update Ejercicio 1 ifelse.cpp

--- Ejercicio 1 ifelse.cpp
+++ Ejercicio 1 ifelse.cpp
@@ -1,7 +1,7 @@
-/*Un hombre desea saber cu·nto dinero se genera por concepto de intereses 
-sobre la cantidad que tiene en inversiÛn en el banco. 
-Decidir· reinvertir los intereses siempre y cuando Èstos excedan a $7000, 
-y en ese caso desea saber cu·nto dinero tendr· finalmente en su cuenta.*/
+/*Un hombre desea saber cu√°nto dinero se genera por concepto de intereses 
+sobre la cantidad que tiene en inversi√≥n en el banco. 
+Decidir√° reinvertir los intereses siempre y cuando √©stos excedan a $7000, 
+y en ese caso desea saber cu√°nto dinero tendr√° finalmente en su cuenta.*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<conio.h>
@@ -16,15 +16,12 @@ int main(){
 	scanf("%f",&tasa);
 	interes=inv*tasa;
 	total=inv+interes;
-	if(interes>7000)
-	{
+	if(interes>7000) {
 	    system("cls");
 	    system("color A0");
 		printf("\n\n\tLa cantidad generada por intereses es: %.0f ",interes); puts(",superan los 7000 pesos");
 		printf("\n\n\tEl saldo generado en la cuenta es: %.0f ", total); puts("pesos");
-	}
-	else
-	{
+	} else {
 	    system("cls");
 	    system("color A0");
 		printf("\n\n\tLa cantidad generada por intereses es: %.0f ", interes); puts(", no superan los 7000 pesos");
