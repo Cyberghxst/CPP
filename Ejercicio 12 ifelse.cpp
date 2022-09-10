3a0d27e776aecc2aaa43f60f52a3b95b6704f620
CGX <92705245+Cyberghxst@users.noreply.github.com>
2022-09-09 19:52:10 -0500

    Update Ejercicio 12 ifelse.cpp

--- Ejercicio 12 ifelse.cpp
+++ Ejercicio 12 ifelse.cpp
@@ -1,7 +1,7 @@
-/*Una compaÒÌa de seguros est· abriendo un departamento de finanzas y estableciÛ un programa para captar clientes, 
-que consiste en lo siguiente: si el monto por el que se efect˙a la pÛliza es menor que $50.000 la cuota a pagar ser· por el 3% del monto, 
-y si el monto es mayor que $50.000 la cuota a pagar ser· el 2% del monto. 
-La compaÒÌa desea determinar cu·l ser· la cuota que debe pagar un cliente.*/
+/*Una compa√±√≠a de seguros est√° abriendo un departamento de finanzas y estableci√≥ un programa para captar clientes, 
+que consiste en lo siguiente: si el monto por el que se efect√∫a la p√≥liza es menor que $50.000 la cuota a pagar ser√° por el 3% del monto, 
+y si el monto es mayor que $50.000 la cuota a pagar ser√° el 2% del monto. 
+La compa√±√≠a desea determinar cu√°l ser√° la cuota que debe pagar un cliente.*/
 #include<stdio.h>
 #include<conio.h>
 #include<windows.h>
@@ -10,17 +10,14 @@ int main(){
 	float monto,pago;
 	setlocale(LC_CTYPE, "Spanish");
 	system("color 5F");
-	printf("\n\t\t\tIntrduzca el monto de la pÛliza ");
+	printf("\n\t\t\tIntrduzca el monto de la p√≥liza ");
 	scanf("%f",&monto);
-	if(monto>50000)
-	{
+	if(monto>50000) {
 		pago=monto*0.02;
 		system("cls");
 		system("color 10");
 		printf("\n\t\t\tLa cuota por pagar del cliente es %.0f ",pago); puts("correspondiente al 2 porciento.");
-	}
-	else
-	{
+	} else {
 		pago=monto*0.03;
 		system("cls");
 		system("color 10");
